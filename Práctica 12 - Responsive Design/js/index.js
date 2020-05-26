1e85d6676920cdf70753f2b83a7b880f2be608e1
Meli <50457885+Zarlie@users.noreply.github.com>
2020-05-26 20:01:08 +0100

    Add files via upload

---
+++ Práctica 12 - Responsive Design/js/index.js
@@ -0,0 +1,6 @@
+// Barra de navegación
+var sidenavElems = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sidenavElems, {});
+
+var parallaxElems = document.querySelectorAll(".parallax");
+M.Parallax.init(parallaxElems, {});
